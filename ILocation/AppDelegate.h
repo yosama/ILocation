937863ec65e69a8ad01521a5166e3df13bddcbee
Yosamac <yosamac@gmail.com>
2015-02-10 14:18:09 +0100

    Initial Commit

---
+++ ILocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ILocation
+//
+//  Created by Yosnier on 10/02/15.
+//  Copyright (c) 2015 YOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
